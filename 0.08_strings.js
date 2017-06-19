aa0193d90caa4ef1c05e2b8d595e213687332e80
Nathaniel Scott Bailey <nathanielscottbailey@Nathaniels-MacBook-Air.local>
2017-06-18 22:49:00 -0400

    added js library

---
+++ 0.08_strings.js
@@ -0,0 +1,9 @@
+//example of a string 
+console.log('Howdy this is a string')
+//strings can be added together
+console.log('Hi' + ' ' + 'I am Nathan')
+//strings can also be added to work with variables
+var cat= 'pusscat'
+console.log('howdy there' + ' ' + cat)
+//.length tells the length of a string inside a variable
+console.log(cat.length)
