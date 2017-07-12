d30f9928746bdc1f6ce68dd1709c5eda9eac914f
Nathaniel Scott Bailey <nathanielscottbailey@Nathaniels-MacBook-Air.local>
2017-07-12 00:56:38 -0400

    added var

---
+++ 0.15var.js
@@ -0,0 +1,13 @@
+var cat = "brown"
+//this was the norma way to write a variable
+//which is a global variable, and very unsecure
+
+//through the newest updates on js we can make several different variables
+
+let dog = "white"
+
+//^ the above variable only lets the key word dog to be defined in a scope
+
+const rat= 'blue'
+
+//const allows us to make a variable thats constant and cannot be changed
